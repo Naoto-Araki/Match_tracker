26ae0491fa4753ed7763097f19cccca3ae60514f
arakinaoto <naoto8705@gmail.com>
2025-04-01 12:51:18 +0900

    Add README

--- match_tracker/core/models.py
+++ match_tracker/core/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+class CustomUser(AbstractUser):
+    nickname = models.CharField(max_length=50, blank=True)
+
 class GameType(models.Model):
     name = models.CharField(max_length=100)
     
