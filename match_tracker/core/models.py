d5d1d906e03cf6617b0c2692ab5a03da60debe83
arakinaoto <naoto8705@gmail.com>
2025-04-01 13:06:43 +0900

    CustomUserの設定

--- match_tracker/core/models.py
+++ match_tracker/core/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     nickname = models.CharField(max_length=50, blank=True)
-
+    
+    def __str__(self):
+        return self.username if not self.nickname else self.nickname
+    
 class GameType(models.Model):
     name = models.CharField(max_length=100)
     
--- match_tracker/match_tracker/settings.py
+++ match_tracker/match_tracker/settings.py
@@ -15,6 +15,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'core.CustomUser'
 
 # Application definition
 
