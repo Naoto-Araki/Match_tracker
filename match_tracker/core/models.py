54985016575f1e4e98f641dfca390ea8bd00276f
arakinaoto <naoto8705@gmail.com>
2025-04-02 00:16:12 +0900

    modelsの作成

--- match_tracker/core/models.py
+++ match_tracker/core/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
@@ -11,4 +12,15 @@ class GameType(models.Model):
     
     def __str__(self):
         return self.name
+
+class Match(models.Model):
+    player1 = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='player1_matches')
+    player2 = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='player2_matches')
+    winner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='won_matches')
+    game_type = models.ForeignKey(GameType, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.player1} vs {self.player2} - {self.game_type.name} - {self.date}"
+    
     
