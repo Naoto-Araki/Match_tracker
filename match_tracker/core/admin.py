2bc578a072d52947038c69e10989ff7ac8c2b9fd
arakinaoto <naoto8705@gmail.com>
2025-04-03 17:21:37 +0900

    MTVモデル設定

--- match_tracker/core/admin.py
+++ match_tracker/core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, GameType, Match
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    # 一覧ページに表示する項目
+    list_display = ['username', 'email', 'nickname', 'is_staff']
+
+    # ユーザー詳細ページ（編集画面）に表示する項目
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('nickname',)}),
+    )
+
+    # ユーザー作成画面（add user）に表示する項目
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('nickname',)}),
+    )
+
+# 登録
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(GameType)
+admin.site.register(Match)
--- match_tracker/core/views.py
+++ match_tracker/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Match
 
+def home(request):
+    matches = Match.objects.all().order_by('-date')
+    return render(request, 'home.html', {'matches': matches})
+    
 # Create your views here.
